4b17e552344aa6f524ce531de7537b65cc9c1f73
Masao-Someki <masao.someki@gmail.com>
2026-02-02 10:51:33 -0500

    Fixed CI error

--- espnet2/text/whisper_token_id_converter.py
+++ espnet2/text/whisper_token_id_converter.py
@@ -65,10 +65,10 @@ class OpenAIWhisperTokenIDConverter:
         timestamps = [f"<|{i * 30 / 1500:.2f}|>" for i in range(0, 1501)]
         sc = [speaker_change_symbol] if sot else []
         special_tokens = (
-            self.tokenizer.tokenizer.additional_special_tokens + timestamps + sc
+            self.tokenizer.tokenizer.extra_special_tokens + timestamps + sc
         )
         self.tokenizer.tokenizer.add_special_tokens(
-            dict(additional_special_tokens=special_tokens)
+            dict(extra_special_tokens=special_tokens)
         )
         self.model_type = model_type
 
--- espnet2/text/whisper_tokenizer.py
+++ espnet2/text/whisper_tokenizer.py
@@ -87,10 +87,10 @@ class OpenAIWhisperTokenizer(AbsTokenizer):
         timestamps = [f"<|{i * 0.02:.2f}|>" for i in range(0, 1501)]
         sc = [speaker_change_symbol] if sot else []
         special_tokens = (
-            self.tokenizer.tokenizer.additional_special_tokens + timestamps + sc
+            self.tokenizer.tokenizer.extra_special_tokens + timestamps + sc
         )
         self.tokenizer.tokenizer.add_special_tokens(
-            dict(additional_special_tokens=special_tokens)
+            dict(extra_special_tokens=special_tokens)
         )
 
     def __repr__(self):
