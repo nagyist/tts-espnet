17e49d818d7e7f312782ce62fad9244115de3315
Fhrozen <nyalta21@gmail.com>
2026-02-22 22:08:28 +0900

    Refactor download logic in install_mwerSegmenter.sh to use variables for URLs

--- tools/installers/install_mwerSegmenter.sh
+++ tools/installers/install_mwerSegmenter.sh
@@ -13,7 +13,16 @@ if [[ ! ${unames} =~ Linux && ! ${unames} =~ Darwin ]]; then
     exit 0
 fi
 
-wget --no-check-certificate --tries=3 https://www-i6.informatik.rwth-aachen.de/web/Software/mwerSegmenter.tar.gz
+PRIMARY_URL="https://www-i6.informatik.rwth-aachen.de/web/Software/mwerSegmenter.tar.gz"
+BACKUP_URL="https://huggingface.co/espnet/ci_tools/resolve/main/mwerSegmenter.tar.gz"
+
+if ! wget --no-check-certificate --tries=3 "${PRIMARY_URL}"; then
+    echo "Primary download failed, trying backup URL..."
+    if ! wget --no-check-certificate --tries=3 "${BACKUP_URL}"; then
+        echo "Both primary and backup downloads failed"
+        exit 1
+    fi
+fi
 tar zxvf mwerSegmenter.tar.gz
 rm mwerSegmenter.tar.gz
 
