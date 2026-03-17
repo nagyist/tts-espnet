c014a6ddc2dd476545bde2680af1657f8aa06593
Masao-Someki <masao.someki@gmail.com>
2026-03-17 00:33:37 -0400

    Fixed naming and added integration test script

---
+++ ci/test_integration_espnet3.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+. tools/activate_python.sh
+. tools/extra_path.sh
+
+python="coverage run --append"
+cwd=$(pwd)
+
+gen_dummy_coverage(){
+    touch empty.py
+    ${python} empty.py
+}
+
+python3 -m pip install -e '.[asr]'
+
+cd ./egs3/mini_an4/asr
+gen_dummy_coverage
+echo "==== [ESPnet3] ASR ===="
+source ./path.sh
+run_with_training_config() {
+    local training_config=$1
+    local runner=$2
+    local inference_config=$3
+
+    ln -sfn "${training_config}" conf/training.yaml
+    ${python} "${runner}" \
+        --stages create_dataset train_tokenizer collect_stats train infer measure \
+        --training_config conf/training.yaml \
+        --inference_config "${inference_config}" \
+        --metrics_config conf/metrics.yaml
+    rm -rf exp data
+}
+
+debug_configs=(
+    training_asr_rnn_data_aug.yaml
+    training_asr_rnn.yaml
+    training_asr_streaming.yaml
+    training_asr_transformer.yaml
+    training_asr_transducer.yaml
+)
+
+for training_config in "${debug_configs[@]}"; do
+    run_with_training_config "${training_config}" run.py conf/inference.yaml
+done
+
+# We need seprate inference config for transducer task
+run_with_training_config \
+    training_transducer_asr_conformer_rnnt.yaml \
+    run.py \
+    conf/inference_transducer.yaml
+
+cd "${cwd}"
