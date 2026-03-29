bcbed9d2d91489e9a8d4d3f1d6b040334b146186
Masao-Someki <masao.someki@gmail.com>
2026-03-29 18:10:31 -0400

    Removed debug comment out

--- ci/test_integration_espnet3.sh
+++ ci/test_integration_espnet3.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# set -euo pipefail
+set -euo pipefail
 
-# . tools/activate_python.sh
-# . tools/extra_path.sh
+. tools/activate_python.sh
+. tools/extra_path.sh
 
-# python="coverage run --append"
-python="python"
+python="coverage run --append"
 cwd=$(pwd)
 
 gen_dummy_coverage(){
@@ -14,12 +13,12 @@ gen_dummy_coverage(){
     ${python} empty.py
 }
 
-# python3 -m pip install -e '.[asr]'
+python3 -m pip install -e '.[asr]'
 
-cd ./egs3/mini_an4/asr
+cd ./egs3/mini_an4/asr || exit
 gen_dummy_coverage
 echo "==== [ESPnet3] ASR ===="
-source ./path.sh
+source path.sh
 run_with_training_config() {
     local training_config=$1
     local runner=$2
@@ -31,25 +30,25 @@ run_with_training_config() {
         --training_config conf/training.yaml \
         --inference_config "${inference_config}" \
         --metrics_config conf/metrics.yaml
-    # rm -rf exp data
+    rm -rf exp data
 }
 
-debug_configs=(
+training_configs=(
     training_asr_rnn_data_aug.yaml
-    # training_asr_rnn.yaml
-    # training_asr_streaming.yaml
-    # training_asr_transformer.yaml
-    # training_asr_transducer.yaml
+    training_asr_rnn.yaml
+    training_asr_streaming.yaml
+    training_asr_transformer.yaml
+    training_asr_transducer.yaml
 )
 
-for training_config in "${debug_configs[@]}"; do
+for training_config in "${training_configs[@]}"; do
     run_with_training_config "${training_config}" run.py conf/inference.yaml
 done
 
-# # We need seprate inference config for transducer task
-# run_with_training_config \
-#     training_transducer_asr_conformer_rnnt.yaml \
-#     run.py \
-#     conf/inference_transducer.yaml
+# We need seprate inference config for transducer task
+run_with_training_config \
+    training_transducer_asr_conformer_rnnt.yaml \
+    run.py \
+    conf/inference_transducer.yaml
 
-cd "${cwd}"
+cd "${cwd}" || exit
